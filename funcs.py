891329b44f7a3efb1d08092fa1ed618ddc0687d8
Andrey <yuryev@yandex.ru>
2023-10-15 23:20:52 +0300

    divide

--- funcs.py
+++ funcs.py
@@ -5,6 +5,8 @@ def calc_add(val1=0, val2=0):
 def calc_sub(val1=0, val2=0):
     return val1 - val2
 
+
 def calc_divide(val1=1, val2=1):
+    ''' divide '''
     if val2 != 0:
         return val1 / val2
